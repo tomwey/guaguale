d6db2f3e6fbf421331c94e66501e8d76bcfd2d84
Tomwey <tomwey@163.com>
2012-08-15 23:31:21 +0800

    xxx

--- Capfile
+++ Capfile
@@ -1,5 +1,5 @@
 load 'deploy' if respond_to?(:namespace)
 
-#load 'deploy/assets'
+load 'deploy/assets'
 Dir['vendor/gems/*/recipes/*.rb','vendor/plugins/*/recipes/*.rb'].each { |plugin| load(plugin) }
 load 'config/deploy'
--- config/deploy.rb
+++ config/deploy.rb
@@ -9,6 +9,8 @@ set :user, "railsu"
 #set :scm_passphrase, "tomwey"
 set :use_sudo, false
 set :deploy_to, "/home/railsu/#{application}"
+set :current_public, "/home/railsu/#{application}/current/public"
+set :shared_path, "/home/railsu/#{application}/shared"
 set :deploy_via, :remote_cache
 set :runner, "railsu"
 set :git_shallow_clone, 1
@@ -16,10 +18,9 @@ set :git_shallow_clone, 1
 role :web, "64.120.193.112"
 role :app, "64.120.193.112"
 role :db, "64.120.193.112", :primary => true
-default_run_options[:pty] = true
 
-after "deploy:update", "deploy:migrate"
-after "deploy:migrate", "deploy:symbol_resource"
+ssh_options[:forward_agent] = true
+default_run_options[:pty] = true
 
 namespace :deploy do
   desc "Restart passenger process"
@@ -27,23 +28,11 @@ namespace :deploy do
     run "touch #{current_path}/tmp/restart.txt"
   end
   
-  desc "copy everything to overwrite the shared public folder"
-  task :symbol_resource do
-      #拷贝新拉下来的images/css/jslib,以及目录下的文件
-      run "rm -rf #{shared_path}/images/*"
-      run "cp -r #{current_public}/images/* #{shared_path}/images/"
-      run "rm -rf #{shared_path}/css/*"
-      run "cp -r #{current_public}/css/* #{shared_path}/css/"
-      run "rm -rf #{shared_path}/jslib/*"
-      run "cp -r #{current_public}/jslib/* #{shared_path}/jslib/"
-      run "rm -f #{shared_path}/*.*"
-      run "cp #{current_public}/*.* #{shared_path}"
-      #然后把拉下来的public目录删除掉
-      run "rm -rf #{current_public}"
-      #把共享的share文件夹中
-      run "ln -sf #{shared_path} #{current_public}"
-    end
+  desc "Symlink shared resources on each release - not used"
+  task :symlink_shared, :roles => :app do
+    run "ln -nfs #{shared_path}/config/database.yml #{release_path}/config/database.yml"
+  end
     
 end
-ssh_options[:forward_agent] = true
-# default_run_options[:pty] = true
+
+after 'deploy:update_code', 'deploy:symlink_shared'
