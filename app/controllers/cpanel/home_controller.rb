3b2255585cefb4eb5d44ca71fbc1260d749ff90d
Tomwey <tomwey@163.com>
2012-08-21 22:00:28 +0800

    finished login

--- app/controllers/cpanel/home_controller.rb
+++ app/controllers/cpanel/home_controller.rb
@@ -1,5 +1,7 @@
 
 class Cpanel::HomeController < Cpanel::BaseController
   def index
+    redirect_to cpanel_tickets_path
+    return
   end
 end
--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,7 @@
 
 class HomeController < ApplicationController
   def index
+    redirect_to new_user_session_url
+    return
   end
 end
